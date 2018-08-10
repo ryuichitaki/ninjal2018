d8bd3cccfabf649f570fdeebd44dc694ad58817c
ryuichitaki <42263274+ryuichitaki@users.noreply.github.com>
2018-08-10 15:37:07 +0900

    Add files via upload

--- www/assets/js/config.js
+++ www/assets/js/config.js
@@ -1,20 +1,19 @@
 
 
-waldayu.transducers["gitksan_compare"] = (function() {
-    var correspondences = {'g̲': 'g', 'x̲': 'x', 'k̲': 'k', 'g_': 'g', 'x_': 'x', 'k_': 'k', 'aa': 'a', 'ii': 'i', 'ee': 'e', 'oo': 'o', 'uu': 'u', "t'": 't', "k'": 'k', "p'": 'p', "ḵ'": 'k', "kw'": 'kw', "ts'": 'ts', "'l": 'l', "'n": 'n', "'m": 'm', "'w": 'w', "'y": 'y'};
-    var keys = ["ḵ'", "kw'", "ts'", 'g̲', 'x̲', 'k̲', 'g_', 'x_', 'k_', 'aa', 'ii', 'ee', 'oo', 'uu', "t'", "k'", "p'", "'l", "'n", "'m", "'w", "'y"];
+waldayu.transducers["kagoshima japanese_compare"] = (function() {
+    var correspondences = {'tsu': 'tu', 'shi': 'si', 'chi': 'ti'};
+    var keys = ['tsu', 'shi', 'chi'];
     var regex = new RegExp("(" + keys.join('|') + ")", 'g');
     return function(str) {
         return str.replace(regex, function(a,b) {
             return correspondences[a];
         });
     };
-})();
-var config = {
+})();var config = {
     "L1": {
-        "name": "Gitksan",
+        "name": "Kagoshima Japanese",
         "underlying": "",
-        "compare": "gitksan_compare",
+        "compare": "kagoshima japanese_compare",
         "lettersInLanguage" : ['a', 'aa', 'b', 'd', 'e', 'ee', 'g', 'g̲', 'gw', 'gy', 'h', 'hl', 'i', 'ii', 'j', 'k', 'k̲', "k'", "k̲'", 'kw', "kw'", 'ky', "ky'", 'l', "'l", 'm', "'m", 'n', "'n", 'o', 'oo', 'p', "p'", 's', 't', "t'", 'ts', "ts'", 'u', 'uu', 'w', "'w", 'x', 'x̲', 'xw', 'y', "'y", "'"]
     },
     "L2": {
